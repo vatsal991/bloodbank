e660048fd6589b2cc4e49cd752f962574ab86b47
vatsal991 <vatsalagola991@gmail.com>
2022-05-06 18:35:00 +0530

    5

--- src/Component/slider.jsx
+++ src/Component/slider.jsx
@@ -17,23 +17,29 @@ export default class SliderComp extends Component {
     return (
       <div style={{height:'50vh',width:'90%',margin:'auto',marginTop:'20px',zIndex:'10'}}>
         <Slider {...settings}>
+          <div>
           <div className="SLiderContainer">
             <div className="slidertext">
               <h4 className="SliderText">Save Lives !</h4>
               <h6>Blood Donation Is A Small Act Of Kindness That Does Great And Big Wonders.</h6>
               </div>
           </div>
+          </div>
+          <div>
           <div className="SLiderContainer">
             <div className="slidertext">
               <h4 className="SliderText">Help To Someone !</h4>
               <h6>Blood Donation Costs You Nothing, But It Can Mean The World To Someone In Need.</h6>
               </div>
           </div>
+          </div>
+          <div>
           <div className="SLiderContainer">
             <div className="slidertext">
               <h4 className="SliderText">Make Other Happy !</h4>
               <h6>Donate Blood Today So That No One Ever Has To Suffer From A Lack Of Blood.</h6>
               </div>
+          </div>
           </div>
         </Slider>
       </div>
